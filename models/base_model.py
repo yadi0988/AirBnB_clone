beb55644a3fa3450d3c3f18e926b2c562d658882
yadi09 <yadamzerterefe09@gmail.com>
2024-03-21 10:56:50 +0100

    ..

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""class that defines all common attributes/methods for other classes"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """BaseModel class"""
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = {}
+        for key, value in self.__dict__.items():
+            if type(value) is datetime:
+                obj_dict[key] = value.isoformat()
+            else:
+                obj_dict[key] = value
+        obj_dict['__class__'] = __class__.__name__
+        return obj_dict
+
+    
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,56 @@
+"Unittest for base_model module"
+
+from models.base_model import BaseModel
+import unittest
+import datetime
+
+def setUpModule():
+    pass
+
+def tearDownModule():
+    pass
+
+class test_baseModel(unittest.TestCase):
+
+    def test_existance_of_attr(self):
+        obj = BaseModel()
+        self.assertTrue(hasattr(obj, "id"))
+        self.assertTrue(hasattr(obj, "created_at"))
+        self.assertTrue(hasattr(obj, "updated_at"))
+        
+    def test_id(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.assertNotEqual(obj1.id, obj2.id)
+
+    def test_is_id_string(self):
+        obj = BaseModel()
+        self.assertTrue(type(obj.id) is str)
+
+    def test_created_at_type(self):
+        obj = BaseModel()
+        self.assertTrue(type(obj.created_at) is datetime.datetime)
+
+    def test_updated_at_type(self):
+        obj = BaseModel()
+        self.assertTrue(type(obj.updated_at) is datetime.datetime)
+
+    def test_str_type(self):
+        obj = BaseModel()
+        self.assertTrue(type(obj.__str__()) is str)
+
+    def test_save_updates_updated_at(self):
+        obj = BaseModel()
+        obj.save()
+        self.assertGreater(obj.updated_at, obj.created_at)
+
+    def test_to_dict_method(self):
+        obj = BaseModel()
+        _dict = obj.to_dict()
+        
+        self.assertTrue(type(_dict) is dict)
+
+        self.assertIn('__class__', _dict)
+
+        self.assertTrue(type(_dict['created_at']) is str)
+        self.assertTrue(type(_dict['updated_at']) is str)
