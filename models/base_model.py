eb612f21855576a3c68432a3320e73e187c6ea4d
yadi09 <yadamzerterefe09@gmail.com>
2024-03-23 16:01:10 +0100

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -21,6 +22,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         "__str__ function"
@@ -31,7 +33,7 @@ class BaseModel():
     def save(self):
         "updates the public instance attribute updated_at"
         self.updated_at = datetime.now()
-        pass
+        models.storage.save()
 
     def to_dict(self):
         """
