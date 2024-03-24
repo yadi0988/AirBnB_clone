e75259066da28ba2fd7116125b789ab773963e6b
yadi09 <yadamzerterefe09@gmail.com>
2024-03-25 00:15:00 +0100

    ......

--- console.py
+++ console.py
@@ -90,9 +90,8 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, cls_name=None):
         empty = True
         objs_list = []
-        c_name = cls_name.replace(".", "")
         try:
-            Cname = globals()[c_name]
+            Cname = globals()[cls_name]
         except Exception:
             if cls_name != '':
                 print("** class doesn't exist **")
